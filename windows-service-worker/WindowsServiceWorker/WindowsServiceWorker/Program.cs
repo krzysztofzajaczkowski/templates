be823d01024a177efc7a67b25af052f5a1c59b2a
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2021-11-30 20:14:19 +0100

    #1 Add comments describing used mechanisms

--- windows-service-worker/WindowsServiceWorker/WindowsServiceWorker/Program.cs
+++ windows-service-worker/WindowsServiceWorker/WindowsServiceWorker/Program.cs
@@ -15,11 +15,21 @@ namespace WindowsServiceWorker
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureAppConfiguration(builder =>
-                    builder.AddJsonFile("custom-config.json", false))
+                {
+                    // appsettings.json is added by default
+                    builder.AddJsonFile("custom-config.json", false);
+                })
                 .ConfigureServices((hostContext, services) =>
                 {
+                    // Configure service provider for automatic dependency injection through constructors
+                    
+                    // Logger is registered by default
+
+                    // Bind config sections to options classes and register them as IOptions<T>
                     services.Configure<ApplicationOptions>(hostContext.Configuration.GetSection("Application"));
                     services.Configure<CustomOptions>(hostContext.Configuration.GetSection("Custom"));
+
+                    // Add background service that executes automatically
                     services.AddHostedService<Worker>();
                 })
                 .UseWindowsService();
--- windows-service-worker/WindowsServiceWorker/WindowsServiceWorker/Worker.cs
+++ windows-service-worker/WindowsServiceWorker/WindowsServiceWorker/Worker.cs
@@ -19,10 +19,23 @@ namespace WindowsServiceWorker
             IOptions<CustomOptions> customOptions,
             IHostApplicationLifetime applicationLifetime)
         {
+            // Typed ILogger<T> is used for logging messages with context (full type name)
             _logger = logger;
-            _applicationLifetime = applicationLifetime;
+
+            // IOptions<T> interface is a wrapper for bound options classes
             _applicationOptions = applicationOptions.Value;
             _customOptions = customOptions.Value;
+
+            // IHostApplicationLifetime interface is used to shut down application
+            // Standard "return" does not work in BackgroundService's ExecuteAsync
+            _applicationLifetime = applicationLifetime;
+        }
+
+        public override Task StartAsync(CancellationToken cancellationToken)
+        {
+            // Called before ExecuteAsync
+            _logger.LogInformation("Custom logic in StartAsync");
+            return base.StartAsync(cancellationToken);
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
@@ -45,6 +58,13 @@ namespace WindowsServiceWorker
                         DateTimeOffset.Now.ToString(_applicationOptions.DateFormat));
                 }
             }
+        }
+
+        public override Task StopAsync(CancellationToken cancellationToken)
+        {
+            // Called after commanding application shutdown
+            _logger.LogInformation("Custom logic in StopAsync");
+            return base.StopAsync(cancellationToken);
         }
     }
 }
